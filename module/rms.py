fa1d1be5ad14d44173fe7c7fc9c4e238c0bac7f0
Yihe <lyh.next@gmail.com>
2025-06-26 23:08:53 -0700

    Add scale parameter to RMSNorm.

--- module/rms.py
+++ module/rms.py
@@ -6,7 +6,13 @@ class RMSNorm(nn.Module):
     eps: float = 1e-5
 
     @nn.compact
-    def __call__(self, x):
-        # TODO: use the scale parameter
-        rms = jnp.rsqrt(jnp.mean(x**2, axis=-1, keepdims=True) + self.eps)
-        return x * rms
+    def __call__(self, x: jax.Array) -> jax.Array:
+        """
+        Apply RMSNorm to the last dimension of the input.
+        """
+
+        scale = self.param("scale", nn.initializers.zeros_init(), (x.shape[-1]))
+
+        r_rms = jnp.rsqrt(jnp.mean(x**2, axis=-1, keepdims=True) + self.eps)
+
+        return x * r_rms * (scale + 1)
