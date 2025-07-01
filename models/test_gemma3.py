eae847d2d7bbc4af0c10dce2e367d5245b76c62d
Yihe <lyh.next@gmail.com>
2025-06-30 22:53:06 -0700

    add the tokenizer - however the output is gibberish

--- models/test_gemma3.py
+++ models/test_gemma3.py
@@ -1,20 +1,24 @@
 import jax
 import jax.numpy as jnp
 from module.transformer import Transformer
-from module.config import GEMMA_3_1B_CONFIG
-from models.gemma3 import load_gemma3_params
+from models.gemma3 import load_gemma3_params, GEMMA_3_1B_CONFIG
+import sentencepiece as spm
 
 GEMMA_3_1B_PATH = "/Users/liuyihe/Models/gemma-3-flax-gemma3-1b-it-v1/gemma3-1b-it"
-
+TOKENIZER_PATH = "/Users/liuyihe/Models/gemma-3-flax-gemma3-1b-it-v1/tokenizer.model"
 
 class TestGemma3:
     def test_gemma3_basics(self):
         model = Transformer(config=GEMMA_3_1B_CONFIG)
         params = load_gemma3_params(path=GEMMA_3_1B_PATH)
 
-        dummy_tokens = jnp.ones((1, 10), dtype=jnp.int32)
-        dummy_mask = jnp.ones((1, 10, 10), dtype=jnp.bool_)
+        tokenizer = spm.SentencePieceProcessor(model_file=TOKENIZER_PATH)
+        tokens = jnp.array([tokenizer.encode_as_ids("Hello, world! This is a test.")], dtype=jnp.int32)
+        mask = jnp.ones((1, tokens.shape[1], tokens.shape[1]), dtype=jnp.bool_)
+        position = jnp.array([list(range(tokens.shape[1]))], dtype=jnp.int32)
 
-        output = model.apply({"params": params}, dummy_tokens, dummy_mask)
+        logits = model.apply({"params": params}, tokens, mask, position)
+        assert logits.shape == (1, tokens.shape[1], GEMMA_3_1B_CONFIG.vocab_size)
 
-        assert output.shape == (1, 10, GEMMA_3_1B_CONFIG.vocab_size)
+        # TODO: Fix the nonsense output.
+        print("Output: ", tokenizer.decode_ids(logits[0].argmax(axis=-1).tolist()))
