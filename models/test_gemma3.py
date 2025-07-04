a53aaa9bf88acd0267f61349a9495cd4d3cb5c55
Yihe <lyh.next@gmail.com>
2025-07-03 23:04:04 -0700

    Update the test for debugging, some debugging notes: * add_bos=True is needed * check output ditribution - jax.nn.softmax(logits) * jax.lax.top_k(logits, 10)
    If the prompt has only *one* token, the model
    generates very *similar* logits as the office
    implementation. However, for long prompts, this
    model starts repeating prompts.
    
    Looks like the attention implementation has an
    issue.

--- models/test_gemma3.py
+++ models/test_gemma3.py
@@ -4,7 +4,8 @@ from module.transformer import Transformer
 from models.gemma3 import load_gemma3_params, GEMMA_3_1B_CONFIG
 import sentencepiece as spm
 
-GEMMA_3_1B_PATH = "/Users/liuyihe/Models/gemma-3-flax-gemma3-1b-it-v1/gemma3-1b-it"
+GEMMA_3_1B_IT_PATH = "/Users/liuyihe/Models/gemma-3-flax-gemma3-1b-it-v1/gemma3-1b-it"
+GEMMA_3_1B_PATH = "/Users/liuyihe/Models/gemma-3-flax-gemma3-1b-v1/gemma3-1b"
 TOKENIZER_PATH = "/Users/liuyihe/Models/gemma-3-flax-gemma3-1b-it-v1/tokenizer.model"
 
 def generate(model, params, tokenizer, text):
@@ -22,13 +23,28 @@ class TestGemma3:
         params = load_gemma3_params(path=GEMMA_3_1B_PATH)
 
         tokenizer = spm.SentencePieceProcessor(model_file=TOKENIZER_PATH)
-        tokens = jnp.array([tokenizer.encode_as_ids("This is ")], dtype=jnp.int32)
+        text = "I'm a language model, "
+        tokens = jnp.array([tokenizer.encode_as_ids(text)], dtype=jnp.int32)
         mask = jnp.tril(jnp.ones((1, tokens.shape[1], tokens.shape[1]), dtype=jnp.bool_))
         position = jnp.array([list(range(tokens.shape[1]))], dtype=jnp.int32)
 
         logits = model.apply({"params": params}, tokens, mask, position)
         assert logits.shape == (1, tokens.shape[1], GEMMA_3_1B_CONFIG.vocab_size)
 
-        # TODO: Fix the nonsense output.
-        print("Output: ", tokenizer.decode_ids(logits[0].argmax(axis=-1).tolist()))
-        assert False
+
+if __name__ == "__main__":
+    model = Transformer(config=GEMMA_3_1B_CONFIG)
+    params = load_gemma3_params(path=GEMMA_3_1B_PATH)
+
+    tokenizer = spm.SentencePieceProcessor(model_file=TOKENIZER_PATH)
+    # text = "Hello"
+    text = "Eiffel tower is located in"
+    for i in range (5):
+        tokens = jnp.array([tokenizer.encode(text, add_bos=True)], dtype=jnp.int32)
+        mask = jnp.tril(jnp.ones((1, tokens.shape[1], tokens.shape[1]), dtype=jnp.bool_))
+        position = jnp.array([list(range(tokens.shape[1]))], dtype=jnp.int32)
+
+        logits = model.apply({"params": params}, tokens, mask, position)
+        _id = logits[0, -1, :].argmax(axis=-1).tolist()
+        text += tokenizer.decode_ids(_id)
+        print(text)
